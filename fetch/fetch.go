4062af2b4e46d2647ac1bdbbaa1e99db50f38743
Paul Khomich <paulkhomich@outlook.com>
2019-10-20 23:45:29 +0300

    fetch

--- fetch/fetch.go
+++ fetch/fetch.go
@@ -5,16 +5,22 @@ import (
 	"os"
 	"fmt"
 	"io"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url;
+		}
+
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Error: %s", err)
 			os.Exit(1)
 		}
 
+		fmt.Println(resp.Status)
 		_, err = io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 		if err != nil {
