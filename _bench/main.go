fd4a3882edcbd6285935352d55ba667c1d430c26
Paul Khomich <paulkhomich@outlook.com>
2020-01-15 00:29:09 +0300

    bench

--- _bench/main.go
+++ _bench/main.go
@@ -1,21 +1,41 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
+	"sync"
 )
 
 func main() {
-	num := make(chan string)
+	integers := make(chan int)
 
-	go hi(num)
+	go printer(integers)
 
-	fmt.Println(<-num)
+	integers <- 3
+	integers <- 2
+	integers <- 6
+	close(integers)
 }
 
-func hi(ch chan string) {
-	r := bufio.NewScanner(os.Stdin)
-	r.Scan()
-	ch <-r.Text()
+func printer(s <-chan int) {
+	ch := make(chan int)
+
+	go func() {
+		var wg sync.WaitGroup
+
+		for i := range s {
+			wg.Add(1)
+			go func(number int) {
+				defer wg.Add(-1)
+				ch <- number * number
+			}(i)
+		}
+
+		go func() {
+			wg.Wait()
+			close(ch)
+		}()
+	}()
+
+	for v := range ch {
+		println(v)
+	}
 }
